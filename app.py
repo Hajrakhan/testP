81fb2d6cf592423ec2224178985765463d0643fd
Hajrakhan <40039884+Hajrakhan@users.noreply.github.com>
2021-08-14 10:12:05 -0700

    Update app.py

--- app.py
+++ app.py
@@ -146,4 +146,179 @@ def search_route():
     posts=get_posts()
     return render_template('search.html',posts=posts,results=results,params=params,category=category,search_term=Term)
 
+# Admin
+@app.route('/admin/')
+def admin_login():
+    return render_template('admin/index.html')
+
+@app.route('/login',methods=['POST','GET'])
+def login():
+    user=request.form['username']
+    password=request.form['password']
+    cur=con.cursor()
+    cur.execute("select * from _user where userpassword=%s AND username=%s",[password,user])
+    temp = cur.fetchall()     
+    cur.close()
+    if temp==[]:
+        error="username and Password not matched"
+        return render_template('admin/index.html',error=error)
+    else:
+        session['logged_in'] = True
+        session['role']=temp[0][5]
+        session['id']=temp[0][0]
+        session['user'] =user
+        return redirect('/home')
+# admin posts
+@app.route('/home')
+def home():
+    if session.get('logged_in') == True:
+        posts=get_posts()
+        return render_template('admin/home.html',posts=posts)
+    else:
+        return render_template('admin/index.html')
+
+@app.route('/add_post')
+def add_post():
+    category=categories()
+    return render_template('admin/add-post.html',cate=category)
+
+def update_category_posts(cat):
+    cur=con.cursor()
+    cur.execute('SELECT categoryposts FROM category WHERE categoryid=%s',[cat])
+    number=cur.fetchall()
+    cur.close()
+    return number
+
+def update_category_posts2(cat,number):
+    cur=con.cursor()
+    cur.execute('UPDATE category SET categoryposts=%s WHERE categoryid=%s',[number,cat])
+    cur.close()
+    return number
+
+@app.route('/add_post_save',methods=['POST'])
+def add_post_save():
+    title=request.form['post_title']
+    postdesc=request.form['postdesc']
+    category=request.form['category']
+    slid=request.files['slider'].filename
+    file2=request.files['file'].filename
+    add_post_function(title,postdesc,category,slid,file2)
+    number=update_category_posts(category)
+    no=number[0][0]
+    update_category_posts2(category,no+1)
+    return redirect('/home')
+
+@app.route('/update-post/<post_slug>/<cat>')
+def updatepost(post_slug,cat):
+    news=get_news(post_slug)
+    return render_template('admin/update-post.html',news=news)
+
+@app.route('/update_post_save',methods=['POST'])
+def update_post_save():
+    id=request.form['post_id']
+    title=request.form['post_title']
+    postdesc=request.form['postdesc']
+    update_post(id,title,postdesc)
+    return redirect('/home')
+
+@app.route('/delete-post/<post_slug>/<cat>')
+def postdelete(post_slug,cat):
+    cur=con.cursor()
+    cur.execute('DELETE FROM post WHERE postid=%s',[post_slug])
+    cur.close()
+    number=update_category_posts(cat)
+    no=number[0][0]
+    update_category_posts2(cat,no-1)
+    return redirect('/home')
+
+# ////// admin posts end
+
+# /////////// admin user
+@app.route('/admin_users')
+def admin_users():
+    users=_users()
+    return render_template('admin/users.html',users=users)
+
+@app.route('/add_user')
+def add_user():
+    return render_template('admin/add-user.html')
+
+@app.route('/add_user_save',methods=['POST'])
+def add_user_save():
+    fname=request.form['fname']
+    lname=request.form['lname']
+    username=request.form['username']
+    password=request.form['password']
+    role=request.form['role']
+    add_user_function(fname,lname,username,password,role)
+    return redirect('admin_users')
+
+@app.route('/update-user/<post_slug>')
+def updateuser(post_slug):
+    cate=user_details(post_slug)
+    return render_template('admin/update-user.html',user=cate)
+
+@app.route('/user_update_save',methods=['POST'])
+def update_user_save():
+    id=request.form['id']
+    fname=request.form['fname']
+    lname=request.form['lname']
+    user=request.form['username']
+    update_user(id,fname,lname,user)
+    return redirect('admin_users')
+
+
+    
+@app.route('/delete-user/<post_slug>')
+def userdelete(post_slug):
+    cur=con.cursor()
+    cur.execute('DELETE FROM _user WHERE userid=%s',[post_slug])
+    cur.close()
+    return redirect('/admin_users')
+
+# //////
+@app.route('/admin_category')
+def admin_category():
+    if session.get('logged_in') == True:
+        category=categories()
+        return render_template('admin/category.html',category=category)
+    else:
+        return render_template('admin/index.html')
+@app.route('/update-category/<post_slug>')
+def categorydetails(post_slug):
+    cate=category_details(post_slug)
+    return render_template('admin/update-category.html',category=cate)
+
+@app.route('/category_update_save',methods=['POST'])
+def update_category_save():
+    id=request.form['cat_id']
+    name=request.form['cat_name']
+    update_category(id,name)
+    return redirect('admin_category')
+@app.route('/add_category')
+def add_category():
+    return render_template('admin/add-category.html')
+@app.route('/add_category_save',methods=['POST'])
+def add_category_save():
+    name=request.form['cat']
+    add_category_function(name)
+    return redirect('admin_category')
+    
+@app.route('/delete-category/<post_slug>')
+def categorydelete(post_slug):
+    cur=con.cursor()
+    cur.execute('DELETE FROM category WHERE categoryid=%s',[post_slug])
+    cur.close()
+    return redirect('/admin_category')
+
+
+
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    session.pop('role', None)
+    session.pop('user', None)
+    return render_template('admin/index.html')
 
