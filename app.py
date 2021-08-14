dad578c056ed3844327884b841833aa1eb8f0950
Hajrakhan <40039884+Hajrakhan@users.noreply.github.com>
2021-08-14 09:26:14 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import psycopg2
 import json
 
@@ -6,7 +6,20 @@ with open('config.json', 'r') as c:
     params = json.load(c)["params"]
 app = Flask(__name__)
 con = psycopg2.connect(database="d7kuslrveqbabl", user="lyawunaedvwsuv", password="29f7357bdc32e4f28a0b5a3bb6e629e60a672805e7ea7dbaac586ddbae722fbc", host="ec2-44-195-209-130.compute-1.amazonaws.com", port="5432")
-
+def get_posts():
+    cur = con.cursor()
+    cur.execute("SELECT * FROM post	LEFT JOIN category ON post.postcategory=category.categoryID LEFT JOIN _user ON post.postAuthor=_user.userID ORDER BY post.postID DESC;")
+    posts = cur.fetchall()
+    cur.close()
+    return(posts)
+def categories():
+    cur = con.cursor()
+    cur.execute("SELECT  * FROM category;")
+    cate=cur.fetchall()
+    return(cate)
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def Home():
+    category=categories()
+    posts=get_posts()
+    return render_template('index.html',params=params,category=category,posts=posts)
+
