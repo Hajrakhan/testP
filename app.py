86552853be295ab95f3be02490098e0376b24d82
Hajrakhan <40039884+Hajrakhan@users.noreply.github.com>
2021-08-14 09:24:10 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+import psycopg2
+import json
 
+with open('config.json', 'r') as c:
+    params = json.load(c)["params"]
 app = Flask(__name__)
+con = psycopg2.connect(database="d7kuslrveqbabl", user="lyawunaedvwsuv", password="29f7357bdc32e4f28a0b5a3bb6e629e60a672805e7ea7dbaac586ddbae722fbc", host="ec2-44-195-209-130.compute-1.amazonaws.com", port="5432")
 
 @app.route("/")
 def hello_world():
